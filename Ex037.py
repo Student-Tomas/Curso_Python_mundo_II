7515ffd0b8112168fa77d7cdb5f077492159b958
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-01-25 15:21:24 -0300

    Curso de python, mundo II, até o exercício 037

--- Ex037.py
+++ Ex037.py
@@ -1,1 +1,16 @@
 # Desafio 037: converção de números para binário, octal e hexadecimal
+num = int(input('Diga um número: '))
+print('''Escolha uma das opções de conversão abaixo:
+[ 1 ] converter para binário
+[ 2 ] converter para octal
+[ 3 ] converter para hexadecimal''')
+opção = int(input('Sua escolha: '))
+if opção == 1:
+    print('{} convertido para binário é {}'.format(num, bin(num)[2:]))
+elif opção == 2:
+    print('{} convertido para octal é {}'.format(num, oct(num)[2:]))
+elif opção == 3:
+    print('{} convertido para hexagonal é {}'.format(num, hex(num)[2:]))
+else:
+    print('Você escolheu uma opção inválida')
+
