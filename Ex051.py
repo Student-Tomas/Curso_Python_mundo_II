68a80c3f8ff1bb92d4f9dcae119494ed74dd6fd2
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-02-03 16:05:54 -0300

    Curso de python, mundo II, até o exercício 0051 enriquecido

--- Ex051.py
+++ Ex051.py
@@ -1,1 +1,8 @@
-# Challenge number 051: terms of an arithmatic progression.
+# Challenge number 051: 10 terms of an arithmatic progression.
+start = int(input('Say the first term: '))
+ratio = int(input('Say the ratio: '))
+tenth = start + (10 - 1) * ratio
+for x in range(start, tenth + ratio, ratio):
+    print('{}'.format(x), end=" -> ")
+print("Done !!!!")
+
