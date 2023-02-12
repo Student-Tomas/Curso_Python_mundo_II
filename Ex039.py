2edcfd29b1c14f2ecea94dcb1875facf605028ed
Student-Tomas <tomasoliveiradealmeida@gmail.com>
2023-02-12 17:07:06 -0300

    Up to exercise number 056

--- Ex039.py
+++ Ex039.py
@@ -1,5 +1,6 @@
 # Desafio 039: alistamento militar de acordo com o ano de nascimento
 from sys import exit
+from datetime import date
 sexo = str(input('''Escolha uma das opções de sexo abaixo:  
 [ M ] para MULHER
 [ H ] para HOMEM
@@ -12,20 +13,22 @@ elif sexo.upper() == "H":
 else:
     print('Opção inválida')
     exit()
-ano_nasc = int(input('Qual o seu ano de nascimento? '))
-from datetime import date
-ano_atual = date.today().year
-alistamento = ano_nasc + 18
-dif_a_menos = alistamento - ano_atual
-dif_a_mais = ano_atual - alistamento
-if alistamento - 1 > ano_atual:
-    print('Você vai se alistar em {}. Ainda faltam {} anos.'.format(alistamento, dif_a_menos))
-elif dif_a_menos == 1:
-        print('Você vai se alistar em {}. Ainda falta {} ano.'.format(alistamento, dif_a_menos))
-elif ano_atual > alistamento + 1:
-    print('Você deveria ter se alistado em {}. Já se passaram {} anos.'.format(alistamento, dif_a_mais))
-elif dif_a_mais == 1:
-    print('Você deveria ter se alistado em {}. Já se passou {} ano'.format(alistamento, dif_a_mais))
+ano_nasc = (input('Qual o seu ano de nascimento? '))
+if ano_nasc.isdigit() and len(ano_nasc) == 4:
+    ano_nasc = int(ano_nasc)
+    ano_atual = date.today().year
+    alistamento = ano_nasc + 18
+    dif_a_menos = alistamento - ano_atual
+    dif_a_mais = ano_atual - alistamento
+    if alistamento - 1 > ano_atual:
+        print('Você vai se alistar em {}. Ainda faltam {} anos.'.format(alistamento, dif_a_menos))
+    elif dif_a_menos == 1:
+            print('Você vai se alistar em {}. Ainda falta {} ano.'.format(alistamento, dif_a_menos))
+    elif ano_atual > alistamento + 1:
+        print('Você deveria ter se alistado em {}. Já se passaram {} anos.'.format(alistamento, dif_a_mais))
+    elif dif_a_mais == 1:
+        print('Você deveria ter se alistado em {}. Já se passou {} ano'.format(alistamento, dif_a_mais))
+    else:
+        print('{} é o seu ano de alistamento. Apresente-se a uma junta militar logo!'.format(ano_atual))
 else:
-    print('{} é o seu ano de alistamento. Apresente-se a uma junta militar logo!'.format(ano_atual))
-
+    print('You have to enter a four digits valid year')
--- Ex056.py
+++ Ex056.py
@@ -1,14 +1,28 @@
 # Challenge number 056: create a code that reads the name, age and gender of four people and, at the end, shows the average age of the group,
 # how many women are less than 20 and what is the name of the oldest man.
 sum_age = 0
+oldest_man_age = 0
+oldest_man_name = ' '
+total_women_20 = 0
 for person in range(1, 5):
     print('----- PERSON NUMBER {} -----'.format(person))
     name = str(input('Name: ')).strip().upper()
     age = int(input('Age: '))
     gender = str(input('Gender: ')).strip().upper()
     sum_age += age
-    if gender == 'M' or gender == 'W':
+    if person == 1 and gender == "M":
+        oldest_man_age = age
+        oldest_man_name = name
+    if gender == "M" and age > oldest_man_age:
+        oldest_man_age = age
+        oldest_man_name = name
+    if gender == "W" and age < 20:
+        total_women_20 += 1
+    if gender == "M" or gender == 'W':
         print("\033[31mLet's go to the next person.\033[m")
     else:
         print('\033[31mGender option invalid! Please, choose only M or W.\033[m')
 print(f'The group average age is {sum_age / 4}')
+print('The oldest man is {}, who is {} years old.'.format(oldest_man_name, oldest_man_age))
+print("We have {} women younger than 20.".format(total_women_20))
+
