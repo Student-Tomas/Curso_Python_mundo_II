4b2d8498ac3bedc48920f179e5267764d5cdb807
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-02-13 18:09:53 -0300

    Python course, world II, up to exercise 059

---
+++ Ex059.py
@@ -0,0 +1,11 @@
+# Challenge 059: create a Optinos Menu. Enter two numbers and say what you want to do with then.
+ num1 = str(input("first number: "))
+ num2 = str(input("Second number: "))
+ while option in "1234":
+   option = int(input('
+[1] sum
+[2] multiplication
+[3] greater
+[4] new numbers
+[5] exit
+>>>>>>> What is your option? ')
