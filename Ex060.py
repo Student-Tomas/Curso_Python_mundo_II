9093d5e7416eb9ff7c4fe9a53a892d82eea0c0c7
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-02-16 15:15:08 -0300

    Python course, world II, up to exercise 060

--- Ex060.py
+++ Ex060.py
@@ -5,9 +5,27 @@ num = int(input("Say the number: "))
 fac = factorial(num)
 print('The factorial of {} is {}'.format(num, fac))'''
 
+# Now, using "while"
+'''num = int(input('Say a number: '))
+c = num
+fac = 1
+while c > 0:
+    print("{}".format(c), end=" ")
+    if c > 1:
+        print(("X"), end=" ")
+    else:
+        print(("="), end=" ")
+    fac *= c
+    c -= 1
+print(fac)'''
+
+# I'm gonna try to solve using "for"
 num = int(input('Enter a number: '))
-seq = num
-for i in range(6, 0):
-    seq = num * i
-print(seq)
+c = num #this is just a counter
+fac = 1
+for i in range(1, num + 1):
+    fac *= c
+    c -= 1
+print("The factorial of {} is {}".format(num, fac))
+
 
