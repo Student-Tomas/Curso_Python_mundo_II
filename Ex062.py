559ac7b92db1f69fc6fc74b186e554bdbe631c58
Student-Tomas <tomasoliveiradealmeida@gmail.com>
2023-02-17 23:37:16 -0300

    Up to exercise number 062

---
+++ Ex062.py
@@ -0,0 +1,12 @@
+# Challenge 062? Emprrove the ex 061 including a option for the user to ask for mor terms, until the user give up.
+first_term = int(input("Say a number to be the first term of the AP: "))
+terms = int(input("Say the initial number of terms: "))
+ratio = int(input("Enter the AP's ratio: "))
+while terms != 0:
+    while terms > 0:
+        print("{} -> ".format(first_term), end=" ")
+        first_term += ratio
+        terms -= 1
+    print("Pause!!!")
+    terms = int(input("How many more terms do you want? "))
+print("The end!!!")
