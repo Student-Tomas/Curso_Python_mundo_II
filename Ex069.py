8cededd03582f0ba7cd9f127976fb28a4d9bbb51
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-02-27 16:50:52 -0300

    Python course, world II, up to exercise 069

--- Ex069.py
+++ Ex069.py
@@ -3,17 +3,24 @@
 age_counter = male_counter = female_counter = 0
 while True:
     age = int(input("What is the age? "))
-    gender = str(input("what is the gender? Answer (M) for male and (F) for female. ")).strip().upper()[0]
+    gender = " "
+    while gender not in "FM":
+        gender = str(input("what is the gender? Answer (M) for male and (F) for female. ")).strip().upper()[0]
     print("~"*30)
-    another_pearson = str(input("Do you want to continue? Yes (Y) or No (N) ")).strip().upper()[0]
-    print("~" * 30)
-    if another_pearson == "N":
-        break
+    if age >= 18:
+        age_counter += 1
     if gender == "F" and age < 20:
         female_counter += 1
     if gender == "M":
         male_counter += 1
-    if age > 18:
-        age_counter += 1
+    another_pearson = " "
+    while another_pearson not in "YN":
+        another_pearson = str(input("Do you want to continue? Yes (Y) or No (N) ")).strip().upper()[0]
+    print("~" * 30)
+    if another_pearson == "N":
+        break
 print("~"*30)
-print(f"We have {age_counter} people who are more than 18. \nIn total we have {male_counter} men, and {female_counter} women are less than 20. ")
+print(f"We have {age_counter} people who are more than 18. ")
+print(f"We have {male_counter} men.")
+print(f"And {female_counter} women are less than 20. ")
+
