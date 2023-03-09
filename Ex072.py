69adf44e71f365035e6bf0608d7da7c3c729bb1c
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-03-09 10:52:12 -0300

    Python course, world II, up to exercise 074

--- Ex072.py
+++ Ex072.py
@@ -3,22 +3,13 @@ list = ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 
 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
 'nineteen', 'twenty')
 while True:
-    num = int(input("Say a number between 0 and 20: "))
-    if 0 <= num <= 20:
-        break
-    print('Try again!')
+    while True:
+        num = int(input("Say a number between 0 and 20: "))
+        if 0 <= num <= 20:
+            break
+        print('Try again!')
     print(f"You said the number {list[num]}")
-    quest = str(input('Do you want to ask again? Y or N')).strip().upper()[0]
+    #while True:
+    quest = str(input('Do you want to ask again? Y or N? ')).strip().upper()[0]
     if quest == "N":
-        break
-
-
-
-
-
-
-
-
-0
-while num < 0 or num > 20:
-    num = int(input("Say a number between 0 and 20: "))
+         break
